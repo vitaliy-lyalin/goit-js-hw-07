c4f5a2b72ae0ca62a44b817df0b86527dbf1f1f0
Vitaliy Lyalin <vitaliy.lyalin@gmail.com>
2023-01-05 22:31:09 +0200

    Add galleryItems markup,  styles and links of simple-lightbox library for 02-lightbox.

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.js-gallery');
+
+createGalleryItemsMarkup();
+
+function createGalleryItemsMarkup() {
+  const markup = galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`;
+    })
+    .join('');
+
+  galleryContainer.insertAdjacentHTML('beforeend', markup);
+}
